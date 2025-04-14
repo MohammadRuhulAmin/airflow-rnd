7974fe814dd31618dd6eb07aaf9e70e4801932fc
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2025-04-14 21:47:49 +0600

    folder added in dags file

---
+++ dags/etl-task-1/taskflow-a.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'ruhul',
+    'retries': 4,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+    dag_id='dag_a_v_3',
+    default_args=default_args,
+    description='Example 1',
+    start_date=datetime(2025, 4, 14),
+    schedule_interval='@daily' 
+) as dag:
+
+    task_1 = BashOperator(
+        task_id='first_task',  
+        bash_command='echo task-1 Executed!'  
+    )
+    task_2 = BashOperator(
+        task_id='second_task',
+        bash_command='echo task-2 Executed!'
+    )
+    task_3 = BashOperator(
+        task_id = 'third_task',
+        bash_command='echo task-3 Executed!'
+    )
+    task_4 = BashOperator(
+        task_id = 'forth_task',
+        bash_command = 'echo task-4 Executed!'
+    )
+    task_5 = BashOperator(
+        task_id = 'fifth_task',
+        bash_command = 'echo task-5 Executed!'
+    )
+
+    task_1.set_downstream(task_2)
+    task_1.set_downstream(task_3)
+    task_2.set_downstream(task_4)
+    task_4.set_downstream(task_5)
+    
